142e99292bed2f8a5776d0d096b9fb3434d616a4
Olesya-Gerasimova <ogerasimova966@gmail.com>
2023-01-13 23:11:01 +0300

    Добавила таблицу Games

--- DotNetExamProject/DatabaseContext.cs
+++ DotNetExamProject/DatabaseContext.cs
@@ -7,6 +7,7 @@ public class DatabaseContext : DbContext
 {
     private readonly IConfiguration _config;
     public DbSet<User> Users { get; set; }
+    public DbSet<Game> Games { get; set; }
 
     public DatabaseContext(DbContextOptions<DatabaseContext> options, IConfiguration config) : base(options)
     {
@@ -22,5 +23,8 @@ public class DatabaseContext : DbContext
     {
         modelBuilder.Entity<User>()
             .HasKey(u => u.Username);
+        modelBuilder.Entity<Game>()
+            .Property(f => f.Id)
+            .ValueGeneratedOnAdd();
     }
 }
