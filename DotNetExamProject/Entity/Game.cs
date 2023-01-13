f2f941a905f61d72b994f88d7ca5127d69ee01a0
Olesya-Gerasimova <ogerasimova966@gmail.com>
2023-01-13 23:10:41 +0300

    Создала Game entity

---
+++ DotNetExamProject/Entity/Game.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetExamProject.Entity;
+
+public class Game
+{
+    [Key]
+    public int Id { get; set; }
+    public string? OwnerUsername { get; set; }
+    public DateTime CreatedOn { get; set; }
+    // game state started | open
+    public GameState State { get; set; }
+}
+
+public enum GameState
+{
+    Started,
+    Open
+}
