ce8d61b4b0f3809c8c53f37842ffdaedbf37fd78
Olesya-Gerasimova <ogerasimova966@gmail.com>
2023-01-13 14:33:51 +0300

    Добавила контроллер регистрации

---
+++ DotNetExamProject/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetExamProject.Controllers;
+
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+[ApiController]
+public class RegisterController : ControllerBase
+{
+    private readonly DatabaseContext _context;
+
+    public RegisterController(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> PostUserModel(User userModel)
+    {
+        _context.Users.Add(userModel);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+}
