23acead9231621d3a87813e23e5689e4057cd88b
Olesya-Gerasimova <ogerasimova966@gmail.com>
2023-01-13 17:56:24 +0300

    Добавила swagger

--- DotNetExamProject/Program.cs
+++ DotNetExamProject/Program.cs
@@ -9,15 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // add postgres
 
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DatabaseContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DatabaseContext>();
 
 // add jwt creation and checking
 
@@ -50,5 +47,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
